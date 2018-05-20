0786eee9d022bedd3e478c992a836aa0894a75ec
Wael Mejdoub <wael.mejdoub@l-mobile.intern>
2018-05-20 17:18:01 +0100

    initial commit

--- InventoryManagement/App_Start/BundleConfig.cs
+++ InventoryManagement/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace InventoryManagement
 								"~/Scripts/respond.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-								"~/Content/bootstrap.css",
+								"~/Content/bootstrap-lumen.css",
+								"~/Content/bootstrap-theme.css",
 								"~/Content/site.css"));
 		}
 	}
